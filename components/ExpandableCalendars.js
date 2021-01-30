34ad3434853d3eecedcc7009466fe817ffa914b0
Jack Piper <jackajpiper@gmail.com>
2021-01-30 18:57:35 +0000

    list scrolls to today successfully

--- components/ExpandableCalendars.js
+++ components/ExpandableCalendars.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
-import {Platform, Alert, StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
+import {SectionList, Alert, StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
 import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
 import moment from "moment";
+import {LinearGradient} from 'expo-linear-gradient';
 
 const testIDs = require('../testIDs');
 
@@ -10,14 +11,14 @@ const today = moment().format("YYYY-MM-DD");
 const themeColor = '#00AAAF';
 const lightThemeColor = '#EBF9F9';
 
-var Symptoms = [
+const Symptoms = [
   { id: 1, name: 'Headache', colour: '#ff00ff' },
   { id: 2, name: 'Nosebleed', colour: '#0000ff' },
   { id: 3, name: 'Lethargy', colour: '#ffaabb' },
   { id: 4, name: 'Wisdom', colour: '#ffff00' }
 ];
 
-var SymptomInstances = [
+const SymptomInstances = [
   { id: 1, typeId: 1, date: '2021-01-06', startTime: '11:40', endTime: '15:05', severity: '30' },
   { id: 2, typeId: 2, date: '2021-01-11', startTime: '19:00', endTime: '19:25', severity: '69' },
   { id: 3, typeId: 3, date: '2021-01-18', startTime: '01:30', endTime: '07:00', severity: '45' },
@@ -40,12 +41,13 @@ var SymptomInstances = [
 
 // takes the symptoms and symptom instances straight from the datastore and converts them for use
 function processInstances(instances, symptoms) {
-  var dateDict = {};
-  var dateArr = [];
+  let dateDict = {};
+  let dateArr = [];
   instances.forEach(function (instance, index) {
-    var symptom = symptoms.find(symptom => symptom.id === instance.typeId);
+    let symptom = symptoms.find(symptom => symptom.id === instance.typeId);
     instance['symptom'] = symptom.name;
-    var day = instance.date;
+    instance['colour'] = symptom.colour;
+    let day = instance.date;
     if (!dateDict[day]) {
       dateDict[day] = [];
     }
@@ -54,6 +56,8 @@ function processInstances(instances, symptoms) {
 
   if (!dateDict[today]) {
     dateDict[today] = [{}];
+  } else {
+    dateDict[today].push({});
   }
 
   Object.keys(dateDict).forEach(function(date) {
@@ -70,11 +74,71 @@ function processInstances(instances, symptoms) {
   return dateArr;
 }
 
-const convertedInstances = processInstances(SymptomInstances, Symptoms);
-// console.log("converted instances are:");
-// console.log(convertedInstances);
+function shadeColor(color, percent) {
+
+  //    ff 00 00
+  // => aa 44 44
+
+  var R = parseInt(color.substring(1,3),16);
+  var G = parseInt(color.substring(3,5),16);
+  var B = parseInt(color.substring(5,7),16);
+
+  let mag = Math.sqrt(R*R + G*G + B*B);
+  R = (R / mag) * 255;
+  G = (G / mag) * 255;
+  B = (B / mag) * 255;
+
+  R = parseInt(R * (100 + percent) / 100);
+  G = parseInt(G * (100 + percent) / 100);
+  B = parseInt(B * (100 + percent) / 100);
+
+  if(percent > 0) {
+    if(R !== 0) {
+      if(G === 0) {
+        G = Math.floor(R * percent / 100);
+      }
+      if(B === 0) {
+        B = Math.floor(R * percent / 100);
+      }
+    }
+    if(G !== 0) {
+      if(R === 0) {
+        R = Math.floor(G * percent / 100);
+      }
+      if(B === 0) {
+        B = Math.floor(G * percent / 100);
+      }
+    }
+    if(B !== 0) {
+      if(G === 0) {
+        G = Math.floor(B * percent / 100);
+      }
+      if(B === 0) {
+        R = Math.floor(B * percent / 100);
+      }
+    }
+  }
+
+  R = (R<255)?R:255;  
+  G = (G<255)?G:255;  
+  B = (B<255)?B:255;  
+
+  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+  return "#"+RR+GG+BB;
+}
+
+
+const ITEMS = processInstances(SymptomInstances, Symptoms);
 
 export default class ExpandableCalendarScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.agendaList = React.createRef();
+  }
 
   buttonPressed() {
     Alert.alert('show more');
@@ -97,23 +161,35 @@ export default class ExpandableCalendarScreen extends Component {
       return this.renderToday();
     }
 
+    let colour = item.colour;
+    let lighterColour = shadeColor(colour, 60);
+    let darkerColour = shadeColor(colour,-60);
+
     return (
-      <TouchableOpacity onPress={() => this.itemPressed(item.symptom)} style={styles.item} testID={testIDs.agenda.ITEM}>
-        <View>
-          <Text style={styles.itemHourText}>{item.startTime + ' - ' + item.endTime}</Text>
-          <Text style={styles.itemDurationText}>{item.severity + '%'}</Text>
-        </View>
-        <Text style={styles.itemTitleText}>{item.symptom}</Text>
-        <View style={styles.itemButtonContainer}>
-          <Button color={'grey'} title={'Info'} onPress={this.buttonPressed} />
-        </View>
-      </TouchableOpacity>
+      <LinearGradient 
+        colors={['white', lighterColour]}
+        style = { styles.container }
+        start={{ x: 0.5, y: 0.5 }}
+        end={{ x: 1, y: 0.5 }}>
+
+        <TouchableOpacity onPress={() => this.itemPressed(item.symptom)} style={styles.item} testID={testIDs.agenda.ITEM}>
+          <View>
+            <Text style={styles.itemHourText}>{item.startTime + ' - ' + item.endTime}</Text>
+            <Text style={styles.itemDurationText}>{item.severity + '%'}</Text>
+          </View>
+          <Text style={styles.itemTitleText}>{item.symptom}</Text>
+          <View style={styles.itemButtonContainer}>
+            <Button color={'grey'} title={'Info'} onPress={this.buttonPressed} />
+          </View>
+        </TouchableOpacity>
+      </LinearGradient>
+      
     );
   };
 
   getMarkedDates = () => {
     const marked = {};
-    convertedInstances.forEach(item => {
+    ITEMS.forEach(item => {
       // NOTE: only mark dates with data
       if (item.data && item.data.length > 0 && !_.isEmpty(item.data[0])) {
         marked[item.title] = {marked: true};
@@ -122,81 +198,28 @@ export default class ExpandableCalendarScreen extends Component {
     return marked;
   };
 
-  getTheme = () => {
-    const disabledColor = 'grey';
-
-    return {
-      // arrows
-      arrowColor: 'black',
-      arrowStyle: {padding: 0},
-      // month
-      monthTextColor: 'black',
-      textMonthFontSize: 16,
-      textMonthFontFamily: 'HelveticaNeue',
-      textMonthFontWeight: 'bold',
-      // day names
-      textSectionTitleColor: 'black',
-      textDayHeaderFontSize: 12,
-      textDayHeaderFontFamily: 'HelveticaNeue',
-      textDayHeaderFontWeight: 'normal',
-      // dates
-      dayTextColor: themeColor,
-      textDayFontSize: 18,
-      textDayFontFamily: 'HelveticaNeue',
-      textDayFontWeight: '500',
-      textDayStyle: {marginTop: Platform.OS === 'android' ? 2 : 4},
-      // selected date
-      selectedDayBackgroundColor: themeColor,
-      selectedDayTextColor: 'white',
-      // disabled date
-      textDisabledColor: disabledColor,
-      // dot (marked date)
-      dotColor: themeColor,
-      selectedDotColor: 'white',
-      disabledDotColor: disabledColor,
-      dotStyle: {marginTop: -2}
-    };
-  };
-
   render() {
     return (
       <CalendarProvider
-        date={'2021-01-12'} // TODO: have this be today
-        onDateChanged={this.onDateChanged}
-        onMonthChange={this.onMonthChange}
+        date={today}
         showTodayButton
         disabledOpacity={0.6}
-        // theme={{
-        //   todayButtonTextColor: themeColor
-        // }}
-        // todayBottomMargin={16}
       >
         {this.props.weekView ? (
           <WeekCalendar testID={testIDs.weekCalendar.CONTAINER} firstDay={1} markedDates={this.getMarkedDates()} />
         ) : (
           <ExpandableCalendar
             testID={testIDs.expandableCalendar.CONTAINER}
-            // horizontal={false}
-            // hideArrows
-            // disablePan
-            // hideKnob
-            // initialPosition={ExpandableCalendar.positions.OPEN}
-            // calendarStyle={styles.calendar}
-            // headerStyle={styles.calendar} // for horizontal only
-            // disableWeekScroll
-            // theme={this.getTheme()}
             disableAllTouchEventsForDisabledDays
             firstDay={1}
-            markedDates={this.getMarkedDates()} // {'2019-06-01': {marked: true}, '2019-06-02': {marked: true}, '2019-06-03': {marked: true}};
-            // leftArrowImageSource={require('../img/previous.png')}
-            // rightArrowImageSource={require('../img/next.png')}
+            markedDates={this.getMarkedDates()}
           />
         )}
         <AgendaList
-          sections={convertedInstances}
+          sections={ITEMS}
           extraData={this.state}
           renderItem={this.renderItem}
-          // sectionStyle={styles.section}
+          ref={this.agendaList}
         />
       </CalendarProvider>
     );
@@ -215,7 +238,6 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 20,
-    backgroundColor: 'white',
     borderBottomWidth: 1,
     borderBottomColor: 'lightgrey',
     flexDirection: 'row'
@@ -241,7 +263,7 @@ const styles = StyleSheet.create({
   },
   emptyItem: {
     paddingLeft: 20,
-    height: 80,
+    height: 60,
     justifyContent: 'center',
     borderBottomWidth: 1,
     borderBottomColor: 'lightgrey',
@@ -250,5 +272,8 @@ const styles = StyleSheet.create({
   emptyItemText: {
     color: 'darkgrey',
     fontSize: 14
+  },
+  container: {
+    flex: 1,
   }
 });
---
+++ data.js
@@ -0,0 +1,27 @@
+const Symptoms = [
+    { id: 1, name: 'Headache', colour: '#ff00ff' },
+    { id: 2, name: 'Nosebleed', colour: '#0000ff' },
+    { id: 3, name: 'Lethargy', colour: '#ffaabb' },
+    { id: 4, name: 'Wisdom', colour: '#ffff00' }
+  ];
+  
+  const SymptomInstances = [
+    { id: 1, typeId: 1, date: '2021-01-06', startTime: '11:40', endTime: '15:05', severity: '30' },
+    { id: 2, typeId: 2, date: '2021-01-11', startTime: '19:00', endTime: '19:25', severity: '69' },
+    { id: 3, typeId: 3, date: '2021-01-18', startTime: '01:30', endTime: '07:00', severity: '45' },
+    { id: 4, typeId: 4, date: '2021-01-29', startTime: '12:40', endTime: '12:55', severity: '78' },
+    { id: 5, typeId: 2, date: '2021-01-04', startTime: '21:00', endTime: '22:25', severity: '12' },
+    { id: 6, typeId: 2, date: '2021-01-12', startTime: '14:30', endTime: '21:00', severity: '28' },
+    { id: 1, typeId: 4, date: '2021-01-02', startTime: '11:40', endTime: '15:05', severity: '30' },
+    { id: 2, typeId: 3, date: '2021-01-21', startTime: '19:00', endTime: '19:25', severity: '69' },
+    { id: 3, typeId: 3, date: '2021-01-26', startTime: '01:30', endTime: '07:00', severity: '45' },
+    { id: 4, typeId: 1, date: '2021-01-09', startTime: '12:40', endTime: '12:55', severity: '78' },
+    { id: 5, typeId: 1, date: '2021-01-19', startTime: '21:00', endTime: '22:25', severity: '12' },
+    { id: 6, typeId: 3, date: '2021-01-01', startTime: '14:30', endTime: '23:00', severity: '28' },
+    { id: 1, typeId: 1, date: '2021-02-06', startTime: '11:40', endTime: '15:05', severity: '30' },
+    { id: 2, typeId: 2, date: '2021-02-11', startTime: '19:00', endTime: '19:25', severity: '69' },
+    { id: 3, typeId: 4, date: '2021-02-18', startTime: '01:30', endTime: '07:00', severity: '45' },
+    { id: 4, typeId: 1, date: '2021-02-29', startTime: '12:40', endTime: '12:55', severity: '78' },
+    { id: 5, typeId: 2, date: '2021-02-04', startTime: '21:00', endTime: '22:25', severity: '12' },
+    { id: 6, typeId: 4, date: '2021-02-12', startTime: '14:30', endTime: '22:00', severity: '28' }
+  ]
