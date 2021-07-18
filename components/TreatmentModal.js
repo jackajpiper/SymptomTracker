0f460022f350ede60c9b484e1fbc46426cdcb738
Jack Piper <jackajpiper@gmail.com>
2021-07-18 11:37:13 +0100

    Removed unnecessary functions

--- components/TreatmentModal.js
+++ components/TreatmentModal.js
@@ -10,62 +10,6 @@ import Toast from 'react-native-simple-toast';
 import Slider from '@react-native-community/slider';
 import ColourHelper from './ColourHelper';
 
-function shadeColour(color, percent) {
-  if (color === "cornflowerblue") {
-    color = "#6495ED";
-  }
-
-  var R = parseInt(color.substring(1,3),16);
-  var G = parseInt(color.substring(3,5),16);
-  var B = parseInt(color.substring(5,7),16);
-
-  let mag = Math.sqrt(R*R + G*G + B*B);
-  R = (R / mag) * 255;
-  G = (G / mag) * 255;
-  B = (B / mag) * 255;
-
-  R = parseInt(R * (100 + percent) / 100);
-  G = parseInt(G * (100 + percent) / 100);
-  B = parseInt(B * (100 + percent) / 100);
-
-  if(percent > 0) {
-    if(R !== 0) {
-      if(G === 0) {
-        G = Math.floor(R * percent / 100);
-      }
-      if(B === 0) {
-        B = Math.floor(R * percent / 100);
-      }
-    }
-    if(G !== 0) {
-      if(R === 0) {
-        R = Math.floor(G * percent / 100);
-      }
-      if(B === 0) {
-        B = Math.floor(G * percent / 100);
-      }
-    }
-    if(B !== 0) {
-      if(G === 0) {
-        G = Math.floor(B * percent / 100);
-      }
-      if(B === 0) {
-        R = Math.floor(B * percent / 100);
-      }
-    }
-  }
-
-  R = (R<255)?R:255;
-  G = (G<255)?G:255;
-  B = (B<255)?B:255;
-
-  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-
-  return "#"+RR+GG+BB;
-}
-
 export default class TreatmentModal extends React.Component {
 
   constructor(props) {
